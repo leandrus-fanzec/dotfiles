d5324ec5242eeff5f0686831b1b407b4c819decf
leandrus-fanzec <lucas@xprodu.com>
2025-02-11 22:15:25 -0300

    actualiza emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,8 +1,10 @@
+;; Configuraciones Generales
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (setq inhibit-splash-screen t)
 (scroll-bar-mode -1)
 (blink-cursor-mode -1)
+
 ;; Easy-Kill de buffer whitout modifications
 (global-set-key [(control x) (k)] 'kill-this-buffer)
 
@@ -19,11 +21,44 @@
         (emacs-lisp-docstring-fill-column t))
     (fill-paragraph nil region)))
 
+;; Abre pdf con Evince (Ubuntu)
 (setq org-file-apps
       '(("pdf" . "evince %s")
 	))
 
+;; Line numbers 
+(defun my-display-numbers-hook ()
+  (display-line-numbers-mode 1)
+  )
+(add-hook 'prog-mode-hook 'my-display-numbers-hook)
 
+;; Org config
+(use-package org
+  :ensure nil ; do not try to install it as it is built-in
+  :config
+  (setq org-M-RET-may-split-line '((default . nil)))
+  (setq org-insert-heading-respect-content t)
+  (setq org-log-done 'time)
+  (setq org-log-into-drawer t)
+
+  (setq org-directory "~/.src/docs/")
+  (setq org-agenda-files (list org-directory))
+
+  ;; Learn about the ! and more by reading the relevant section of the
+  ;; Org manual.  Evaluate: (info "(org) Tracking TODO state changes")
+  (setq org-todo-keywords
+        '((sequence "TODO(t)" "WAIT(w!)" "|" "CANCEL(c!)" "DONE(d!)"))))
+
+(add-hook 'org-mode-hook 'turn-on-flyspell)
+(add-hook 'org-mode-hook 'olivetti-mode)
+(setq org-startup-indented t
+      org-pretty-entities t
+      org-adapt-indentation t
+      org-odd-levels-only t
+      org-hide-leading-stars t
+      org-image-actual-width '(300))
+
+;; Packages Repositories
 (require 'package)
 
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
@@ -39,7 +74,8 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
-;; Enable vertico
+;; Packages Setup
+
 (use-package vertico
   :init
   (vertico-mode))
@@ -58,32 +94,110 @@
 (use-package org-modern)
 (global-org-modern-mode)
 
-;; Line numbers 
-(defun my-display-numbers-hook ()
-  (display-line-numbers-mode 1)
-  )
-(add-hook 'prog-mode-hook 'my-display-numbers-hook)
-
 (require 'spacious-padding)
-
 (spacious-padding-mode 1)
-
 ;; Set a key binding if you need to toggle spacious padding.
 (define-key global-map (kbd "<f8>") #'spacious-padding-mode)
 
 ;; Load the theme of your choice.
 (load-theme 'modus-operandi)
-
 (define-key global-map (kbd "<f5>") #'modus-themes-toggle)
 
-(add-hook 'org-mode-hook 'turn-on-flyspell)
-(add-hook 'org-mode-hook 'olivetti-mode)
-(setq org-startup-indented t
-      org-pretty-entities t
-      org-adapt-indentation t
-      org-odd-levels-only t
-      org-hide-leading-stars t
-      org-image-actual-width '(300))
+;; Denote
+(use-package denote)
+(require 'denote)
+
+;; Remember to check the doc strings of those variables.
+(setq denote-directory (expand-file-name "~/.src/docs/"))
+(setq denote-save-buffers nil)
+(setq denote-known-keywords '("emacs" "philosophy" "politics" "economics"))
+(setq denote-infer-keywords t)
+(setq denote-sort-keywords t)
+(setq denote-file-type nil) ; Org is the default, set others here
+(setq denote-prompts '(title keywords))
+(setq denote-excluded-directories-regexp nil)
+(setq denote-excluded-keywords-regexp nil)
+(setq denote-rename-confirmations '(rewrite-front-matter modify-file-name))
+
+;; Pick dates, where relevant, with Org's advanced interface:
+(setq denote-date-prompt-use-org-read-date t)
+
+;; Read this manual for how to specify `denote-templates'.  We do not
+;; include an example here to avoid potential confusion.
+
+(setq denote-date-format nil) ; read doc string
+
+;; By default, we do not show the context of links.  We just display
+;; file names.  This provides a more informative view.
+(setq denote-backlinks-show-context t)
+
+;; Also see `denote-backlinks-display-buffer-action' which is a bit
+;; advanced.
+
+;; If you use Markdown or plain text files (Org renders links as buttons
+;; right away)
+(add-hook 'text-mode-hook #'denote-fontify-links-mode-maybe)
+
+;; Generic (great if you rename files Denote-style in lots of places):
+(add-hook 'dired-mode-hook #'denote-dired-mode)
+;;
+;; OR if only want it in `denote-dired-directories':
+;;(add-hook 'dired-mode-hook #'denote-dired-mode-in-directories)
+
+;; Automatically rename Denote buffers using the `denote-rename-buffer-format'.
+(denote-rename-buffer-mode 1)
+
+;; Denote DOES NOT define any key bindings.  This is for the user to
+;; decide.  For example:
+(let ((map global-map))
+  (define-key map (kbd "C-c n n") #'denote)
+  (define-key map (kbd "C-c n o") #'denote-open-or-create)
+  (define-key map (kbd "C-c n c") #'denote-region) ; "contents" mnemonic
+  (define-key map (kbd "C-c n N") #'denote-type)
+  (define-key map (kbd "C-c n d") #'denote-date)
+  (define-key map (kbd "C-c n z") #'denote-signature) ; "zettelkasten" mnemonic
+  (define-key map (kbd "C-c n s") #'denote-subdirectory)
+  (define-key map (kbd "C-c n t") #'denote-template)
+  ;; If you intend to use Denote with a variety of file types, it is
+  ;; easier to bind the link-related commands to the `global-map', as
+  ;; shown here.  Otherwise follow the same pattern for `org-mode-map',
+  ;; `markdown-mode-map', and/or `text-mode-map'.
+  (define-key map (kbd "C-c n i") #'denote-link) ; "insert" mnemonic
+  (define-key map (kbd "C-c n I") #'denote-add-links)
+  (define-key map (kbd "C-c n b") #'denote-backlinks)
+  (define-key map (kbd "C-c n f f") #'denote-find-link)
+  (define-key map (kbd "C-c n f b") #'denote-find-backlink)
+  ;; Note that `denote-rename-file' can work from any context, not just
+  ;; Dired bufffers.  That is why we bind it here to the `global-map'.
+  (define-key map (kbd "C-c n r") #'denote-rename-file)
+  (define-key map (kbd "C-c n R") #'denote-rename-file-using-front-matter))
+
+;; Key bindings specifically for Dired.
+(let ((map dired-mode-map))
+  (define-key map (kbd "C-c C-d C-i") #'denote-link-dired-marked-notes)
+  (define-key map (kbd "C-c C-d C-r") #'denote-dired-rename-files)
+  (define-key map (kbd "C-c C-d C-k") #'denote-dired-rename-marked-files-with-keywords)
+  (define-key map (kbd "C-c C-d C-R") #'denote-dired-rename-marked-files-using-front-matter))
+
+(with-eval-after-load 'org-capture
+  (setq denote-org-capture-specifiers "%l\n%i\n%?")
+  (add-to-list 'org-capture-templates
+               '("n" "New note (with denote.el)" plain
+                 (file denote-last-path)
+                 #'denote-org-capture
+                 :no-save t
+                 :immediate-finish nil
+                 :kill-buffer t
+                 :jump-to-captured t)))
+
+;; Also check the commands `denote-link-after-creating',
+;; `denote-link-or-create'.  You may want to bind them to keys as well.
+
+
+;; If you want to have Denote commands available via a right click
+;; context menu, use the following and then enable
+;; `context-menu-mode'.
+(add-hook 'context-menu-functions #'denote-context-menu)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -91,7 +205,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(flyspell-default-dictionary "es")
- '(package-selected-packages '(spacious-padding org-modern company orderless vertico)))
+ '(package-selected-packages
+   '(olivetti denote spacious-padding org-modern company orderless vertico)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
